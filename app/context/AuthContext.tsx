837f33fb274e92734b7b05a589d4c9056dda33eb
matiasbermudez <mabgrc@hotmail.com>
2026-01-05 20:54:11 -0300

    intentando el authcontext

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -1,6 +1,36 @@
+'use client';
+import { useState, createContext, useContext } from "react";
 
-export const AuthContext = () => {
+
+type AuthContextType = {
+  isAuthenticated: boolean;
+  login: () => void;
+  logout: () => void;
+};
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => setIsAuthenticated(true);
+  const logout = () => setIsAuthenticated(false);
+
+
+
   return (
-    <div>AuthContext</div>
-  )
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+
+  return context;
 }
